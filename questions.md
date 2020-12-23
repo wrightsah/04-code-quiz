97fe0214c92cecdf4ea2133804f6f83c3d3acf71
Stephen Wright <wrightsah@icloud.com>
2020-12-23 15:18:28 -0500

    created array of objects for questions list

---
+++ questions.md
@@ -0,0 +1,32 @@
+Inside which HTML element would JavaScript be able to run? 
+    1. <javascript>
+    2. <js>
+    3. <script>
+    4. <java> 
+
+One equals sign (=) indicates which of the following? 
+    1. values are equal
+    2. values are the same type and equal
+    3. storing the value of a variable
+    4. a value is true
+
+How would you make "Hello World!" pop up on the screen? 
+    1. alert var = "Hello World!" 
+    2. alert("Hello World!");
+    3. popup("Hello World!");
+    4. window."Hello World!";
+
+Which of the following is a function declaration? 
+    1. function.script
+    2. var function = myFunction
+    3. func myFunction
+    4. function myFunction()
+
+How would you access the first element of an array named cars? 
+    1. cars(1)
+    2. cars[1]
+    3. cars(0)
+    4. cars[0]
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,61 @@
-// create the timer 
+// click button to start timer
 
+// define button
+var startButton = document.getElementById("start-button");
+startButton.addEventListener("click", startQuiz);
 
+// define timer
 
-// create an object for the questions and the answers
+function startQuiz() {
+  console.log("Start button pressed.");
 
-var questions = {
+  var timer = document.getElementById("timer");
+  var time = 75;
 
+  // runs every second
+
+  setInterval(countDown, 1000);
+
+  // function to count down time
+
+  function countDown() {
+    time--;
+    console.log(time);
+    timer.innerHTML = time;
+  }
 }
+
+// create an array of object for the questions and the answers
+
+var questions = [
+    {
+        question: "Inside which HTML element would JavaScript be able to run?", 
+        choices: ["<javascript>", "<js>", "<script>", "<java>"],
+        answer: "<script>" 
+    },
+    {
+        question: "One equals sign (=) indicates which of the following?",
+        choices: ["values are equal", "values are the same type and equal", "storing the value of a variable", "a value is true"],
+        answer: "storing the value of a variable"
+    }, 
+    {
+        question: 'How would you make "Hello World!" pop up on the screen?',
+        choices: ['alert var = "Hello World!"', 'alert("Hello World!");', 'popup("Hello World!");', 'window."Hello World!";'],
+        answer: 'alert("Hello World!");'
+    }, 
+    {
+        question: "Which of the following is a function declaration?",
+        choices: ["function.script", "var function = myFunction", "func myFunction", "function myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        question: "How would you access the first element of an array named cars?",
+        choices: ["cars(1)", "cars[1]", "cars(0)", "cars[0]"], 
+        answer: "cars[0]"
+    }
+];
+    
 
 // create object to store scores and names
 
-var highScores = {
-
-}
+var highScores = {};
